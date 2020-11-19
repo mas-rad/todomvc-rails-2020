5676db31eaf27b80e95be49c6a93c3c78a8ec30d
PoUpA <euphorikdjs@gmail.com>
2020-11-19 19:40:12 +0000

    Ajout du filtre completed

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ApplicationRecord
   scope :active, -> { where(completed: false) }
-
+  scope :completed, -> { where(completed: true) }
   validates :title, presence: true
 
   def title=(string)
